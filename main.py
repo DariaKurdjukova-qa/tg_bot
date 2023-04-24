093193c367cfaf2e33728b94ef4a6a5c01d8ba2f
Daria <127792580+DariaKurdjukova-qa@users.noreply.github.com>
2023-04-24 23:40:35 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,61 @@
+from telebot import TeleBot, types
+
+import wikipedia, re
+
+bot = TeleBot(token='6070218133:AAGFDq01q01T2guBjOSNLlHnMGVKL_4FoUM', parse_mode='html') # создание бота
+
+# обработчик команды '/start'
+@bot.message_handler(commands=['start'])
+def start_command_handler(message: types.Message):
+    # отправляем ответ на команду '/start'
+    bot.send_message(
+        chat_id=message.chat.id, # id чата, в который необходимо направить сообщение
+        text='Приветствую! Я краткая копия русской Википедии. Напиши, о чём бы ты хотел узнать, и я постараюсь помочь!', # текст сообщения
+    )
+
+
+# Устанавливаем русский язык в Wikipedia
+wikipedia.set_lang("ru")
+# Чистим текст статьи в Wikipedia и ограничиваем его тысячей символов
+def getwiki(s):
+    try:
+        ny = wikipedia.page(s)
+        # Получаем первую тысячу символов
+        wikitext=ny.content[:1000]
+        # Разделяем по точкам
+        wikimas=wikitext.split('.')
+        # Отбрасываем всЕ после последней точки
+        wikimas = wikimas[:-1]
+        # Создаем пустую переменную для текста
+        wikitext2 = ''
+        # Проходимся по строкам, где нет знаков «равно» (то есть все, кроме заголовков)
+        for x in wikimas:
+            if not('==' in x):
+                    # Если в строке осталось больше трех символов, добавляем ее к нашей переменной и возвращаем утерянные при разделении строк точки на место
+                if(len((x.strip()))>3):
+                   wikitext2=wikitext2+x+'.'
+            else:
+                break
+        # Теперь при помощи регулярных выражений убираем разметку
+        wikitext2=re.sub('\([^()]*\)', '', wikitext2)
+        wikitext2=re.sub('\([^()]*\)', '', wikitext2)
+        wikitext2=re.sub('\{[^\{\}]*\}', '', wikitext2)
+        # Возвращаем текстовую строку
+        return wikitext2
+    # Обрабатываем исключение, которое мог вернуть модуль wikipedia при запросе
+    except Exception as e:
+        return 'В энциклопедии нет информации об этом'
+# Получение сообщений от юзера
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    bot.send_message(message.chat.id, getwiki(message.text))
+
+
+# главная функция программы
+def main():
+    # запускаем нашего бота
+    bot.infinity_polling()
+
+
+if __name__ == '__main__':
+    main()
